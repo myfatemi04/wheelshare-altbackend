331aa1f88426476a0fe7a84b2039babad9cb8738
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-07-06 21:00:15 -0400

    added requests and invitations to users.ts

--- api/carpools.ts
+++ api/carpools.ts
@@ -1,1 +1,2 @@
-export {};
+import prisma from "./prisma";
+
--- api/invitations.ts
+++ api/invitations.ts
@@ -1,86 +1,36 @@
 import prisma from "./prisma";
 
-export async function create({
-  userId,
-  carpoolId,
-  isRequest,
-}: {
-  userId: number;
-  carpoolId: number;
-  isRequest: boolean;
-}) {
-  const existing = await prisma.invitation.findFirst({
-    where: {
-      userId,
-      carpoolId,
-    },
-  });
-  if (existing) {
-    if (existing.isRequest) {
-      // Do nothing
-    } else {
-      // They were already invited, and then they requested to join
-      execute({ userId, carpoolId });
-    }
-  } else {
-    return await prisma.invitation.create({
-      data: {
-        userId,
-        carpoolId,
-        sentTime: new Date(),
-        isRequest,
-      },
-    });
-  }
+export type InvitationInit = {
+    userId: number;
+    carpoolId: number;
+    isRequest: boolean;
+    sentTime: Date;
 }
 
-export async function execute({
-  userId,
-  carpoolId,
-}: {
-  userId: number;
-  carpoolId: number;
-}) {
-  const existing = await prisma.invitation.findFirst({
-    where: { userId, carpoolId },
-  });
-  if (!existing) {
-    throw new Error("Cannot execute invite/request that does not exist");
-  }
-  await prisma.invitation.delete({
-    where: {
-      userId_carpoolId: {
-        userId,
-        carpoolId,
-      },
-    },
-  });
-  return await prisma.carpool.update({
-    where: {
-      id: carpoolId,
-    },
-    data: {
-      members: {
-        connect: {
-          id: userId,
-        },
-      },
-    },
-  });
+export async function create({
+    userId,
+    carpoolId,
+    isRequest,
+    sentTime
+}: InvitationInit) {
+    
+    return await prisma.invitation.create({
+
+    })
 }
 
 export async function invitations(id: number) {
-  const invitations = prisma.invitation.findMany({
-    select: {
-      userId: true,
-      carpoolId: true,
-      isRequest: true,
-      sentTime: true,
-    },
-    where: {
-      carpoolId: id,
-      userId: id,
-    },
-  });
-  return invitations;
+    const invitations = prisma.invitation.findMany({
+        select: {
+            userId: true,
+            carpoolId: true,
+            isRequest: true,
+            sentTime: true
+        },
+        where: {
+          carpoolId: id,
+          userId: id
+        }
+    });
+    return invitations;
 }
