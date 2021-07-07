958b2ac0092f4e0b5a1c3b8598d2cedeff6d44c7
Nitin Kanchinadam <nitin.kanchinadam@gmail.com>
2021-07-06 20:40:18 -0400

    added method to get all invitations (invitations) in invitations.ts

--- api/invitations.ts
+++ api/invitations.ts
@@ -15,6 +15,22 @@ export async function create({
 }: InvitationInit) {
     
     return await prisma.invitation.create({
-        
+
     })
+}
+
+export async function invitations(id: number) {
+    const invitations = prisma.invitation.findMany({
+        select: {
+            userId: true,
+            carpoolId: true,
+            isRequest: true,
+            sentTime: true
+        },
+        where: {
+          carpoolId: id,
+          userId: id
+        }
+    });
+    return invitations;
 }
