6bc5c4d43b03bbb8b3fa7befbdb38d753b401442
Michael Fatemi <myfatemi04@gmail.com>
2021-08-18 21:48:47 -0400

    fix await

--- src/api/carpools.ts
+++ src/api/carpools.ts
@@ -239,29 +239,37 @@ export async function setNote(carpoolId: number, note: string) {
 	});
 }
 
-export async function pagination(carpoolId: number, beforeMessageTimestamp: Date | null): Promise<Message[]> {
-  if(beforeMessageTimestamp == null) {
+export async function pagination(
+	carpoolId: number,
+	beforeMessageTimestamp: Date | null
+): Promise<Message[]> {
+	if (beforeMessageTimestamp == null) {
 		beforeMessageTimestamp = new Date();
 	}
 
-	const messages = await prisma.carpool.findFirst({
-		select: {
-			messages: true
-		},
-		where: {
-			id: carpoolId
-		}
-	}).messages;
+	const messages = (
+		await prisma.carpool.findFirst({
+			select: {
+				messages: true,
+			},
+			where: {
+				id: carpoolId,
+			},
+		})
+	).messages;
 
 	let index = messages.length;
-	while(messages[index].sentTime.getTime() != beforeMessageTimestamp.getTime() && index != 0) {
+	while (
+		messages[index].sentTime.getTime() != beforeMessageTimestamp.getTime() &&
+		index != 0
+	) {
 		index -= NUMBER_MESSAGES_PAGINATION;
 		index = Math.max(0, index);
 	}
 
 	let start = Math.max(0, index - NUMBER_MESSAGES_PAGINATION);
 	let newMessages: Message[] = Array(index - start);
-	for(let i = start; i < index; i++) {
+	for (let i = start; i < index; i++) {
 		newMessages[i - start] = messages[i];
 	}
 	return newMessages;
